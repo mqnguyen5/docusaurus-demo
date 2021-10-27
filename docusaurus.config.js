39068ffc28207376a26e580e8dd14847c7347a95
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-10-27 18:23:52 -0400

    Update docusaurus.config.js for GitHub pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://mqnguyen5.github.io',
+  baseUrl: '/docusaurus-demo',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mqnguyen5', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-demo', // Usually your repo name.
 
   presets: [
     [
