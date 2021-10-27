418987793238dc57b58d7459f0bc3cf86d402e3b
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-10-27 18:32:47 -0400

    Fixes typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://mqnguyen5.github.io',
-  baseUrl: '/docusaurus-demo',
+  baseUrl: '/docusaurus-demo/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
